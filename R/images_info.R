f2d82bb3ba82c97d189d93c883d3ff24bac714ab
Wlodek Bzyl <matwb@univ.gda.pl>
2020-11-15 23:18:52 +0100

    update R/images_info.R, 10-mr_images_to_tensors.Rmd

--- R/images_info.R
+++ R/images_info.R
@@ -1,11 +1,31 @@
-# images_list <- function(dicom_dir) {
-#   image = "image_base|dicom_color|dicom_red|dicom_blue"
-#
-#   paths = list.files(
-#     path = dicom_dir,
-#     pattern = ".*\\.dcm$",
-#     full.names = TRUE,
-#     recursive = TRUE
-#   ) %>%
-#     stringr::str_subset(image)
-# }
+images_info <- function(dicom_dir, extension = "dcm") {
+  image = "image_base|dicom_color|dicom_red|dicom_blue"
+  splitter = glue::glue("^({dicom_dir})/([0-9]+)/({image})/.*\\.(?:{extension})$")
+
+  tibble::tibble(
+    file_name = fs::dir_ls(
+      path = "../80_images",
+      regexp = glue::glue(".*\\.({extension}$)"),
+      recurse = TRUE
+    ) %>%
+      as.character() %>%
+      stringr::str_subset(image)
+  ) %>%
+    tidyr::extract(
+      col = file_name,
+      into = c("series", "patient", "image"),
+      regex = splitter,
+      remove = FALSE
+    ) %>%
+    dplyr::mutate(
+      kind = dplyr::recode(
+        image,
+        image_base = "base",
+        dicom_color = "fat",
+        dicom_red = "subcutaneous_fat",
+        dicom_blue = "visceral_fat"
+      )
+    ) %>%
+    dplyr::arrange(patient, kind) %>%
+    select(3, 5, 4, 2, 1)
+}
